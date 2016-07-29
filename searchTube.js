3dcaf026ecb676b8d0ffdc21cbee291055ca8d21
IMLaing <ianmlaing@gmail.com>
2016-07-29 17:43:23 -0400

    creating tubeAPI codeacademy

---
+++ searchTube.js
@@ -0,0 +1,25 @@
+//show JSON by using strinify to create a strong that can be used as a variable and added to the innerHTML #response
+$(function(){
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyCX9ZTK48f0vQ5_iPDc1yK37ADI5IUQpmw');    
+}
+
+function search(){
+	var request = gapi.client.youtube.search.list({
+		part: 'id'
+	}); 
+}
+
+function onSearchResponse(response){
+	showResponse(response);
+	}
+});
