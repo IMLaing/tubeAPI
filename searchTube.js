1cd8038c47d5d7678e05ea53daf6c88ed68878ea
IMLaing <ianmlaing@gmail.com>
2016-07-30 12:15:32 -0400

    added in thumbnail and link and title

--- searchTube.js
+++ searchTube.js
@@ -4,13 +4,19 @@
 //once a promise is called, it runs the function once a response is recevied, once it is done it then runs the next function
 //.then will run the promise after the function is completed.
 //used when a request depends on another request
+//We create the function and we let other parts of the code call it for us.
+
 
 var renderSearchResult = function(item){
-	return '<p>' + item.snippet.channelTitle + '</p>';
+	var html = '';
+	html += '<h1>' + item.snippet.title + '</h1>';
+	html += '<p>' + item.snippet.channelTitle + '  <a href="https://www.youtube.com/watch?v='+item.id.videoId+'"><br>  ';
+	html += '<img src="'+ item.snippet.thumbnails.default.url +'" alt="thumbnail"></a></p>';  
+	return html;
 };
 
 $(document).ready(function(){
-
+//  LISTENER --------------------| 
 	$('#search-term').on('submit', function(eventObject){
 		eventObject.preventDefault();
 		var searchQuery = $('#query').val();
